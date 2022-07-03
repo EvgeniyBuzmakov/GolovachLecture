b0faeb87c286d493e84ea3d80b24a0ef3c7cf12c
EvgeniyBuzmakov <evgeniybuzmakov86@gmail.com>
2022-07-03 14:47:39 +0500

    GolovachLecture - Memory in Java

---
+++ src/MemoryInJava/OutOfMemoryError.java
@@ -0,0 +1,22 @@
+package MemoryInJava;
+
+/*
+
+ */
+public class OutOfMemoryError {
+
+    public static void main(String[] args) {
+        Object ref = new long[Integer.MAX_VALUE / 100];
+        Object ref2 = new long[Integer.MAX_VALUE / 100];
+        Object ref3 = new long[Integer.MAX_VALUE / 100];
+        Object ref4 = new long[Integer.MAX_VALUE / 100];
+        Object ref5 = new long[Integer.MAX_VALUE / 10];
+        Object ref6 = new long[Integer.MAX_VALUE / 10];
+        Object ref7 = new long[Integer.MAX_VALUE / 10];
+        Object ref8 = new long[Integer.MAX_VALUE / 10];
+        Object ref9 = new long[Integer.MAX_VALUE / 10];
+        Object ref10 = new long[Integer.MAX_VALUE / 10];
+        Object ref11 = new long[Integer.MAX_VALUE / 10];
+    }
+
+}
